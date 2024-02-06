9f300246ea1b4085bc9485755ee2486adf8fb927
ahmadfraz4 <ahmadfraz2015@gmail.com>
2024-02-06 20:50:41 +0500

    changing client folder

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use("/api/v1",require("./src/routers/OrderRouter"));
 app.use('/api/v1', require('./src/routers/payment.router'))
 
 
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '/client', 'index.html'));
 });
 
 
